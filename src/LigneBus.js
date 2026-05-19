48c14e18a5026e414170dcc662e6bf1dc424375d
Faye Cheikh Sow <fayecheikhsow@gmail.com>
2026-05-19 22:12:03 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/LigneBus.js
@@ -0,0 +1,18 @@
+import './LigneBus.css';
+
+function LigneBus({ numero, depart, arrivee, arrets, estSelectionnee, onClick }) {
+  return (
+    <div
+      className={`ligne-bus ${estSelectionnee ? 'ligne-bus-active' : ''}`}
+      onClick={onClick}
+    >
+      <div className="ligne-numero">{numero}</div>
+      <div className="ligne-info">
+        <span className="ligne-trajet">{depart} → {arrivee}</span>
+        <span className="ligne-arrets">{arrets} arrêts</span>
+      </div>
+    </div>
+  );
+}
+
+export default LigneBus;
